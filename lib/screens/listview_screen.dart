26b40319fa858986ce32d28fc7bf7f1ae9e6314d
Diego Prado <kaoxdc@gmail.com>
2022-08-16 18:28:06 -0500

    list view

---
+++ lib/screens/listview_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ListviewScreen extends StatelessWidget {
+  final peliculas = const [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam',
+    'Ironman',
+  ];
+  const ListviewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Listview Screen')),
+      body: ListView(
+        children: [
+          ...peliculas
+              .map(
+                (peli) => ListTile(
+                  leading: const Icon(Icons.movie),
+                  title: Text(peli),
+                ),
+              )
+              .toList(),
+
+          // ListTile(
+          //   leading: Icon(Icons.photo_album),
+          //   title: Text('Album'),
+          // ),
+          // ListTile(
+          //   leading: Icon(Icons.phone),
+          //   title: Text('Phone'),
+          // ),
+        ],
+      ),
+    );
+  }
+}

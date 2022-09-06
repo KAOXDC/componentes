700f71e53a828033ea46cb78d016402b5a4d58e8
Diego Prado <kaoxdc@gmail.com>
2022-09-06 17:56:51 -0500

    alerts

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,13 +1,107 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('data'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Texto en la Alerta'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Ok'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(10)),
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Texto en la Alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Cancelar',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  'Cancelar',
+                ),
+              ),
+            ]);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Alert Screen")),
-      body: const Center(child: Text("Alert Screen")),
-    );
+        appBar: AppBar(title: const Text("Alert Screen")),
+        body: Center(
+          child: ElevatedButton(
+            // style: ElevatedButton.styleFrom(
+            //   primary: Colors.amber,
+            //   shape: const StadiumBorder(),
+            // ),
+
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Mostrar Alerta', style: TextStyle(fontSize: 16)),
+            ),
+            // onPressed: () => displayDialogIOS(context),
+            onPressed: () => Platform.isAndroid
+                ? displayDialogAndroid(context)
+                : displayDialogIOS(context),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber,
+          child: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ));
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static const Color primary = Colors.deepPurple;
   static final ThemeData darkTheme = ThemeData.light().copyWith(
-      // AppBar
-      appBarTheme: const AppBarTheme(
-        color: primary,
+    // AppBar
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+    // texto botones
+    textButtonTheme:
+        TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
+    // Floating Action button
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 5,
+    ),
+    // Elevated button
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.amber,
+        shape: const StadiumBorder(),
         elevation: 0,
       ),
-      // texto botones
-      textButtonTheme:
-          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)));
+    ),
+  );
 }
